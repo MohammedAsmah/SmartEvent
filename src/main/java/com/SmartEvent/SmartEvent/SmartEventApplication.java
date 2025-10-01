33362e7328a1b1571f399d12e5f537e43c9cc3b0
mohammeasmah <mohammed.asmah@securityeagles.com>
2025-10-01 22:21:56 +0100

    first commit

---
+++ src/main/java/com/SmartEvent/SmartEvent/SmartEventApplication.java
@@ -0,0 +1,13 @@
+package com.SmartEvent.SmartEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartEvent/SmartEvent/SmartEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartEvent.SmartEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
